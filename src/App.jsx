baaab8dd6bdb707ed1e213aa276126c0941ac739
Karl Schoß <karlschoss@Air-von-Karl.fritz.box>
2026-05-20 22:52:18 +0200

    Fix BrowserRouter basename for GitHub Pages subdirectory

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function Home() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/impressum" element={<Impressum />} />
